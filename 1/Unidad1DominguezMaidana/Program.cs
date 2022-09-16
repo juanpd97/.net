6d5ffb931e6b653903fe342a335394352122d456
juan pablo dominguez <juanpdominguez97@gmail.com>
2022-09-16 20:56:09 -0300

    .net tuti

---
+++ 1/Unidad1DominguezMaidana/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidad1Maidana
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaro las variabales que voy a utilizar
+            string apynom = "", direccion = "";
+            long telefono = 0;
+            //solicito el nombre y apellido y lo cargo a apynom
+            Console.WriteLine("Ingrese su Apellido y nombre: ");
+            apynom = Console.ReadLine();
+            //solicito la direccion y lo cargo a direccion
+            Console.WriteLine("Ingrese su dirección: ");
+            direccion = Console.ReadLine();
+            //solicito el teléfono lo cargo a telefono
+            Console.WriteLine("Ingrese su teléfono: ");
+            telefono = EsNumero();
+            //long.TryParse(Console.ReadLine(), out telefono);
+            //mostramos la línea solicitada
+            Console.WriteLine($"La persona: {apynom} vive en dirección: {direccion}. Número Telefónico: {telefono}");
+            // Con cualquier tecla que se presione, se cierra la venta y sale con error 0
+            Console.WriteLine("Fin puntos 1, 2 y 3\nPresione una tecla para continuar\n");
+            Console.ReadKey();
+            // a partir de acá comienza el punto 4
+            Console.WriteLine("Punto 4 - Definir e inicializar una variable de cada tipo (bool, byte, char, decimal, double, float, int, long, short y string) y mostrar por pantalla el contenido de cada uno.\n");
+            bool buleano = true;
+            byte bait = 255;
+            char caracter = 'a';
+            decimal deci = 123;
+            double doble = -10307;
+            float flotante = 345678;
+            int entero = 1234567890;
+            long largo = 1234567890987654321;
+            short corto = 32767;
+            string vcadena = "Cadena";
+            Console.WriteLine($"Booleano: {buleano}");
+            Console.WriteLine($"Byte: {bait}");
+            Console.WriteLine($"Char: {caracter}");
+            Console.WriteLine($"Decimal: {deci}");
+            Console.WriteLine($"Double: {doble}");
+            Console.WriteLine($"Flotante: {flotante}");
+            Console.WriteLine($"Int: {entero}");
+            Console.WriteLine($"Long: {largo}");
+            Console.WriteLine($"Short: {corto}");
+            Console.WriteLine($"String: {vcadena}");
+            Console.WriteLine("Acá termina el punto 4.\nPresione una tecla para continuar\n");
+            Console.ReadKey();
+            // Acá comienza el punto 5
+            Console.WriteLine("Punto 5\n¿Las siguientes variables están bien inicializadas? ¿Cuál es el error de compilación?\n");
+            Console.WriteLine("int numero = string");
+            Console.WriteLine("Error\tCS0029\tNo se puede convertir implícitamente el tipo 'string' en 'int'");
+            Console.WriteLine("string cadena = 1");
+            Console.WriteLine("Error\tCS0029\tNo se puede convertir implícitamente el tipo 'int' en 'string'");
+            Console.WriteLine("Ambas están mal inicializadas, se inician con un tipo de dato (int y string) y se le asigna otro tipo de datos, por este motivo dan error de compilación.\nPresione una tecla para finalizar");
+            Console.ReadKey();
+        }
+        //Con esta función corroboramos que se ingrese un número >
+        static long EsNumero()
+        {
+            long aux_telefono;
+            while (!long.TryParse(Console.ReadLine(), out aux_telefono))
+            {
+                Console.Write("El valor ingresado no es un número.\nIngrese un Número por favor: ");
+            }
+            return aux_telefono;
+        }
+    }
+}
---
+++ 2/unidad 2/unidad 2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unidad_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("----------------------- 2 --------------------\n");
+         //2Hacer un programa que le pida al usuario que ingrese un número y le responda si es divisible por 3.
+
+            //declaro las variables que voy a necesitar
+            int numero;
+
+            Console.Write("ingrese un numero: ");
+            numero = NumeroEntero();
+
+            //utilizo un condicional if para verificar si el numero ingresado es divisible por 3
+            if ( (numero%3) == 0){
+                Console.WriteLine($"el numero {numero} es divisible por 3"); 
+            }else
+            { Console.WriteLine($"el numero {numero} NO es divisible por 3"); }
+
+
+            Console.WriteLine("\n----------------------- 3 --------------------\n");
+            //3 Hacer un programa que le pida al usuario un número del 1 al 12 y
+            //escriba el nombre del mes que corresponda a ese número. Usar Switch
+
+            Console.Write("ingrese un numero entre 1 y 12: ");
+            numero = NumeroEntero();
+
+            //utilizo un while para validar que el numero que ingresa el usuario este dentro del de 1-12
+            while ( (numero>12) || (numero<1) ) {
+                Console.Write("ingrese un numero entre 1 y 12: ");
+                numero = NumeroEntero();
+            }
+
+            switch (numero)
+            {
+                case 1: Console.WriteLine("enero");
+                    break;
+                case 2: Console.WriteLine("febrero");
+                    break;
+                case 3: Console.WriteLine("marzo");
+                    break;
+                case 4: Console.WriteLine("abril");
+                    break;
+                case 5: Console.WriteLine("mayo");
+                    break;
+                case 6: Console.WriteLine("junio");
+                    break;
+                case 7: Console.WriteLine("julio");
+                    break;
+                case 8: Console.WriteLine("agosto");
+                    break;
+                case 9: Console.WriteLine("septiembre");
+                    break;
+                case 10: Console.WriteLine("octubre");
+                    break;
+                case 11: Console.WriteLine("noviembre");
+                    break;
+                case 12: Console.WriteLine("diciembre");
+                    break;
+                default: Console.WriteLine("error");
+                    break;
+            }
+
+
+            Console.WriteLine("\n----------------------- 4 --------------------\n");
+
+            //4 Hacer un programa que muestre la tabla de multiplicar del 1 al 10 de cualquier número. Usar For
+
+            Console.Write("ingrese un numero: ");
+            numero = NumeroEntero();
+            for (int i = 1; i<=10; i++){
+                Console.WriteLine( $" - {numero} * {i} = {i * numero}");
+            }
+
+            //5En los puntos 2,3 y 4 verificar que se haya ingresado un número y
+            //sino se muestre un mensaje de error y vuelva a solicitar el ingreso
+        } 
+        static int NumeroEntero()
+        {
+            int numero;
+            while (!Int32.TryParse(Console.ReadLine(), out numero))
+            {
+                Console.Write("El valor ingresado no es un número.\nIngrese un Número: ");
+            }
+            return numero;
+        }
+    }
+}
