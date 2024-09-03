36c534477b129a297dac5d6e6c615d7449fa2cb7
juanpd97 <juanpdominguez97@gmail.com>
2024-09-03 00:34:07 -0300

    -

---
+++ CRUD-GPT/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_GPT.Models
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public int edad { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public Usuario() { }
+
+    }
+}
